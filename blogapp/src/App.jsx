3dad4cd9c18316cab84d4f496a7d83d9e5222c84
AmanAta <95543969+AmanAta@users.noreply.github.com>
2023-10-31 08:24:51 +0530

    blog

---
+++ blogapp/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+    <h1 className="bg-pink-600 text-purple-200 text-6xl font-bold ">
+      Hello EveryOne this is my First Blog website
+    </h1>
+    </>
+  )
+}
+
+export default App
